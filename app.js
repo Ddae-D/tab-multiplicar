ae33e74844201a91c92ebd82594792cef65cf4ff
Ddae-D <identifiedbyOOO@gmail.com>
2021-08-24 19:31:51 -0500

    configuracion de yargs 1, parametro base

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+const { number } = require('yargs');
 const {crearArchivo} = require('./helpers/multiplicar');
-// *******************************
-const argv = require('yargs').argv;
-//console.log(argv);
-//console.log('base:',argv.base);
-//console.log(process.argv);
-// const [ , , arg3= '-base=1'] = process.argv;
-// ******************************* console.log(base);
+const argv = require('yargs')
+    .option('b',{
+        alias:'base',
+        type:number,
+        demandOption:true
+        })
+        .check((argv,options)=>{
+            if(isNaN(argv.b)){
+                throw 'Base no numerica'
+            }
+            return true;
+        }).argv;
+        
 crearArchivo(argv.base)
 .then(file=>console.log(file,'creado'))
 .catch(error=>console.log(error));
-//********************************* */
